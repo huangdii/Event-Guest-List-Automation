b8b0cfcb5f4c1e181eb6ee2c35b8312805d847cf
huangdii <ubuntu@ip-172-31-22-213.us-east-2.compute.internal>
2021-11-01 00:51:19 +0000

    .

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,10 +14,13 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
+  ### 2021-10-31 #####
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-
+  ActionMailer::Base.default_content_type = "text/html"
+  #####################
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
