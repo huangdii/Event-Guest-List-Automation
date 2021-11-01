41e2baefa9d304964e8d2781b21a4ea3004a67b0
huangdii <ubuntu@ip-172-31-22-213.us-east-2.compute.internal>
2021-11-01 01:35:09 +0000

    heroku adds on

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -1,13 +1,29 @@
+require "uri"
+require "net/http"
+require "json"
 class GuestMailer < ApplicationMailer
     default from: 'notifications@example.com'
     # def rsvp_invitation_email(event, guest)
     def rsvp_invitation_email(guest)
+        url = URI.parse(ENV['TRUSTIFI_URL'] + "/api/i/v1/email")
+        https = Net::HTTP.new(url.host, url.port)
+        https.use_ssl = true
+        
+        request = Net::HTTP::Post.new(url)
+        request["x-trustifi-key"] = ENV['TRUSTIFI_KEY']
+        request["x-trustifi-secret"] = ENV['TRUSTIFI_SECRET']
+        request["content-type"] = "application/json"
+        request.body = "{\"recipients\": [{\"email\": \"hwangdi66@gmail.com\"}],\"title\":\"Title\",\"html\":\"Body\"}"
+    
+        response = https.request(request)
+        puts response.read_body
+        
         # puts @guest.email_address.to_s
         flash[:notice] = "hi hello what's up"
         # @event = event
         @guest = guest
         @url = 'http://www.gmail.com'
-        mail(to: @guest.email, subject: "- Invitation")
+        # mail(to: @guest.email, subject: "- Invitation")
     end
     
     def rsvp_confirmation_email(event, guest)
@@ -16,5 +32,11 @@ class GuestMailer < ApplicationMailer
         
         mail(to: @guest.email_address, subject: "#{@event.title} - Seating Confirmation")
     end
-
+    
+    #########
+    # require "uri"
+    # require "net/http"
+    # require "json"
+    
+        
 end
